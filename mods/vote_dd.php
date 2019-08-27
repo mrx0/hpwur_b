906a3a1273feb8500f0d7f78ae70b6e0e2879e70
mrx0 <alex.vilkov@bk.ru>
2019-08-27 23:26:39 +0300

    ...

---
+++ mods/vote_dd.php
@@ -0,0 +1,63 @@
+<?php
+// Write to log.
+debug_log('vote_level()');
+
+// For debug.
+//debug_log($update);
+//debug_log($data);
+
+// Get action.
+$action = $data['arg'];
+
+// Standart level.
+if ($action == '0') {
+    // Standart users level.
+    my_query(
+        "
+        UPDATE    users
+        SET       level = 8
+          WHERE   user_id = {$update['callback_query']['from']['id']}
+        "
+    );
+}
+
+// Up-vote.
+if ($action == 'up') {
+    // Increase users level.
+
+//    my_query(
+//        "
+//        UPDATE    users
+//        SET       level = IF(level = 0, 30, level+1)
+//          WHERE   user_id = {$update['callback_query']['from']['id']}
+//            AND   level < 40
+//        "
+//    );
+
+    my_query(
+        "
+        UPDATE    users
+        SET       level = IF(level = 0, 8, level+1)
+          WHERE   user_id = {$update['callback_query']['from']['id']}
+            AND   level < 15
+        "
+    );
+}
+
+// Down-vote.
+if ($action == 'down') {
+    // Decrease users level.
+    my_query(
+        "
+        UPDATE    users
+        SET       level = level-1
+          WHERE   user_id = {$update['callback_query']['from']['id']}
+            AND   level > 0
+        "
+    );
+}
+
+// Send vote response.
+send_response_vote($update, $data);
+
+exit();
